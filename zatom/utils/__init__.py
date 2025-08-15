73dd37ccd9090557de6e946e33ba41c4e8e65cf5
Alex Morehead <alex.morehead@gmail.com>
2025-08-15 00:10:45 +0000

    Change src -> zatom, update LICENSE, README, and pyproject and gitignore

---
+++ zatom/utils/__init__.py
@@ -0,0 +1,5 @@
+from zatom.utils.instantiators import instantiate_callbacks, instantiate_loggers
+from zatom.utils.logging_utils import log_hyperparameters
+from zatom.utils.pylogger import RankedLogger
+from zatom.utils.rich_utils import enforce_tags, print_config_tree
+from zatom.utils.utils import extras, get_metric_value, task_wrapper
