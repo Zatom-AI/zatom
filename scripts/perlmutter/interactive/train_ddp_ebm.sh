5afdb0252beb90031b515a5d980228485a344245
Alex Morehead <alex.morehead@gmail.com>
2025-09-03 18:06:16 +0000

    Reset interactive run

--- scripts/perlmutter/interactive/train_ddp_ebm.sh
+++ scripts/perlmutter/interactive/train_ddp_ebm.sh
@@ -34,8 +34,8 @@ NHEAD=12  # 6, 12, 16
 
 # Define run details
 DEFAULT_DATASET="joint"                   # NOTE: Set the dataset to be used, must be one of (`joint`, `qm9_only`, `mp20_only`, `qmof150_only`, `omol25_only`)
-DEFAULT_RUN_ID="qstepcmp"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
-DEFAULT_RUN_DATE="2025-09-02_17-15-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
+DEFAULT_RUN_ID="wd8timmk"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
+DEFAULT_RUN_DATE="2025-09-03_11-15-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
 
 DATASET=${1:-$DEFAULT_DATASET}            # First argument or default dataset if not provided
 RUN_NAME="EBT-M__${DATASET}_subset_s1_debugging"       # Name of the model type and dataset configuration
