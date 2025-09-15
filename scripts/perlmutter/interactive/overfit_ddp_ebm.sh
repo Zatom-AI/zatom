aeebf2f3bf799a4bd42b8b07e3db2aecb81f77ee
Alex Morehead <alex.morehead@gmail.com>
2025-09-15 04:41:57 +0000

    Reset run

--- scripts/perlmutter/interactive/overfit_ddp_ebm.sh
+++ scripts/perlmutter/interactive/overfit_ddp_ebm.sh
@@ -34,8 +34,8 @@ NHEAD=12  # 6, 12, 16
 
 # Define run details
 DEFAULT_DATASET="joint"                   # NOTE: Set the dataset to be used, must be one of (`joint`, `qm9_only`, `mp20_only`, `qmof150_only`, `omol25_only`, `geom_only`)
-DEFAULT_RUN_ID="fcebke25"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
-DEFAULT_RUN_DATE="2025-09-14_20-30-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
+DEFAULT_RUN_ID="du5p6vjn"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
+DEFAULT_RUN_DATE="2025-09-14_21-30-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
 
 DATASET=${1:-$DEFAULT_DATASET}            # First argument or default dataset if not provided
 RUN_NAME="EBT-B__ecoder@768_${DATASET}_overfitting_molecule-and-material_s1_nmsil_jvp-attn-masking"       # Name of the model type and dataset configuration
@@ -107,7 +107,6 @@ bash -c "
     trainer=ddp \
     trainer.accumulate_grad_batches=4 \
     trainer.check_val_every_n_epoch=null \
-    trainer.gradient_clip_val=1.0 \
     trainer.max_epochs=50000 \
     trainer.num_nodes=$SLURM_JOB_NUM_NODES \
     trainer.devices=$SLURM_NTASKS_PER_NODE \
