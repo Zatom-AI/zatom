e29325eb2f5d1e4463215432c91445cf4f4ca35f
Alex Morehead <alex.morehead@gmail.com>
2025-09-29 19:56:08 +0000

    Sync up shared_interactive and regular QOS training job scripts

--- scripts/perlmutter/interactive/train_ddp_mft.sh
+++ scripts/perlmutter/interactive/train_ddp_mft.sh
@@ -34,7 +34,7 @@ NHEAD=12  # 6, 12, 16
 
 # Define run details
 DEFAULT_DATASET="joint"                   # NOTE: Set the dataset to be used, must be one of (`joint`, `qm9_only`, `mp20_only`, `qmof150_only`, `omol25_only`, `geom_only`)
-DEFAULT_RUN_ID="tadx0i5l"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
+DEFAULT_RUN_ID="sw38hx9e"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
 DEFAULT_RUN_DATE="2025-09-29_12-30-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
 
 DATASET=${1:-$DEFAULT_DATASET}            # First argument or default dataset if not provided
