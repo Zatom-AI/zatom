2923cb6f00653a3425d1095bca3d0330987ecf44
Alex Morehead <alex.morehead@gmail.com>
2025-10-25 14:35:09 -0700

    Set up new sweep

--- scripts/perlmutter/regular/sweep_gpu.sh
+++ scripts/perlmutter/regular/sweep_gpu.sh
@@ -7,13 +7,13 @@
 #SBATCH --module=gpu,nccl-plugin                              # load GPU and optimized NCCL plugin modules
 #SBATCH --account=m5008                                       # use specified account for billing (e.g., `m5008_g` for AI4Science proposal, `dasrepo` for all else)
 #SBATCH --nodes=1                                             # NOTE: this needs to match Lightning's `Trainer(num_nodes=...)`
-#SBATCH --gpus-per-node=1                                     # request A100 GPU resource(s)
-#SBATCH --ntasks-per-node=1                                   # NOTE: this needs to be `1` on SLURM clusters when using Lightning's `ddp_spawn` strategy`; otherwise, set to match Lightning's quantity of `Trainer(devices=...)`
-#SBATCH --time=00-19:00:00                                    # time limit for the job (up to 2 days: `02-00:00:00`)
+#SBATCH --gpus-per-node=2                                     # request A100 GPU resource(s)
+#SBATCH --ntasks-per-node=2                                   # NOTE: this needs to be `1` on SLURM clusters when using Lightning's `ddp_spawn` strategy`; otherwise, set to match Lightning's quantity of `Trainer(devices=...)`
+#SBATCH --time=00-23:00:00                                    # time limit for the job (up to 2 days: `02-00:00:00`)
 #SBATCH --job-name=sweep                                      # job name
 #SBATCH --output=scripts/perlmutter/regular/logs/sweep%j.out  # output log file
 #SBATCH --error=scripts/perlmutter/regular/logs/sweep%j.err   # error log file
-#SBATCH --array=0-5                                           # create an array of jobs for the sweep (0-7 or 8 total for training and 0-35 or 36 total for evaluation)
+#SBATCH --array=0-8                                           # create an array of jobs for the sweep (0-8 or 9 total for training and 0-35 or 36 total for evaluation)
 
 # Wait for 5-10 seconds randomly to avoid race condition
 sleep $((RANDOM % 6 + 5))
@@ -34,7 +34,7 @@ mkdir -p "$TORCH_HOME"
 mkdir -p "$HF_HOME"
 
 # Define run details
-DEFAULT_SWEEP_ID="gd0h50vo"                   # NOTE: Generate a unique ID for each run by running `wandb sweep configs/sweep/{train,eval}_sweep_{joint,}.yaml`
+DEFAULT_SWEEP_ID="3v2mw3i1"                   # NOTE: Generate a unique ID for each run by running `wandb sweep configs/sweep/{train,eval}_sweep_{joint,}.yaml`
 SWEEP_ID=${1:-$DEFAULT_SWEEP_ID}              # First argument or default ID if not provided
 
 # Inform user of job details
