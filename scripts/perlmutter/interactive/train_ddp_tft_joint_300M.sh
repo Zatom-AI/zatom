c98660a79f8c1f1f4e2802be6776e7adb9014efc
Alex Morehead <alex.morehead@gmail.com>
2025-12-15 04:25:21 +0000

    Switch to 300M model run

--- scripts/perlmutter/interactive/train_ddp_tft_joint_300M.sh
+++ scripts/perlmutter/interactive/train_ddp_tft_joint_300M.sh
@@ -9,7 +9,7 @@
 #        --gpus-per-node=2 \
 #        --ntasks-per-node=2 \
 #        --time=04:00:00 \
-#        --job-name=tft-160M-joint
+#        --job-name=tft-300M-joint
 
 # Determine location of the project's directory
 # PROJECT_ID="dasrepo"
@@ -32,7 +32,7 @@ DEFAULT_RUN_ID="1me8cgux"                 # NOTE: Generate a unique ID for each 
 DEFAULT_RUN_DATE="2025-12-14_20-30-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
 DEFAULT_MODEL="zatom"                     # NOTE: Set the model to be used, must be one of (`zatom`,)
 DEFAULT_EXPERIMENT="train"                # NOTE: Set the experiment name to be used, must be one of (`train`, `finetune`, `eval`, `overfit`)
-DEFAULT_ARCHITECTURE="tft_160M"           # NOTE: Set the model architecture to be used, must be one of (`{tft,}_70M`, `{tft,}_160M`, `{tft,}_300M`, `{mft,mfp}_80M`, `{mft,mfp}_180M`, `{mft,mfp}_500M`)
+DEFAULT_ARCHITECTURE="tft_300M"           # NOTE: Set the model architecture to be used, must be one of (`{tft,}_70M`, `{tft,}_160M`, `{tft,}_300M`, `{mft,mfp}_80M`, `{mft,mfp}_180M`, `{mft,mfp}_500M`)
 
 DATASET=${1:-$DEFAULT_DATASET}            # First argument or default dataset if not provided
 RUN_ID=${2:-$DEFAULT_RUN_ID}              # Second argument or default ID if not provided
